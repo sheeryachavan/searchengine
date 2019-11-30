d655e984429a5e921a72447d1c4c5f7c44fa6221
spatole12 <spatole@stevens.edu>
2019-11-30 14:18:40 -0500

    config changes

--- backend/config.py
+++ backend/config.py
@@ -3,5 +3,5 @@ from pymongo import MongoClient
 import dns
 
 def MongoConnection(): 
-    client = MongoClient("mongodb+srv://admin:admin@combining-hku7y.mongodb.net/test?retryWrites=true&w=majority")
+    client = MongoClient("mongodb+srv://<username>:<password>@combining-hku7y.mongodb.net/test?retryWrites=true&w=majority")
     return client
