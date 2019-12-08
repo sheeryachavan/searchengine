b9cc68295620163caec14c7eac13ad487bfc9430
spatole12 <spatole@stevens.edu>
2019-12-07 19:33:58 -0500

    try catch and input and or check

--- backend/app.py
+++ backend/app.py
@@ -11,6 +11,7 @@ import numpy as np
 import requests
 from pymongo import MongoClient
 import dns
+import re
 
 # def MongoConnection(): 
 client = MongoClient("mongodb+srv://admin:admin@combining-hku7y.mongodb.net/test?retryWrites=true&w=majority")
@@ -75,78 +76,94 @@ import json
 
 @app.route("/results", methods=["POST"])
 def results():
-    print("hello")
-    print(flask.request.url)
-    print(flask.request.get_data().decode("utf-8"))
-    # print(flask.request.data.decode("utf-8"))
-    allresult,mainresult = dataRetrieval(flask.request.get_data().decode("utf-8"))
-    # print(query)
-    return jsonify(mainresult)
+    try:
+        print("hello")
+        print(flask.request.url)
+        print(flask.request.get_data().decode("utf-8"))
+        # print(flask.request.data.decode("utf-8"))
+        allresult,mainresult = dataRetrieval(flask.request.get_data().decode("utf-8"))
+        # print(query)
+        return jsonify(mainresult)
+    except:
+        print("Recipes not found")
+
 @app.route("/results", methods=["GET"])
 def results_blank():
-    return flask.redirect(flask.url_for('/'), code=200)
+    try:
+        return flask.redirect(flask.url_for('/'), code=200)
+    except:
+        print("Error in redirecting")
 
 
 @app.route("/")
 def index():
-    
-    return flask.redirect("http://sheeryachavan.github.io/searchengine", code=200)
-
+    try:
+        return flask.redirect("http://sheeryachavan.github.io/searchengine", code=200)
+    except:
+        print("Something went wrong!")
 
 def dataRetrieval(query):
-    ingredients_str = query
-    print(query)
-    ingredients_str = ingredients_str.split()
-    stop_words = set(["salt", "pepper"])
-    db = client.foodfood
-    ingredients = [w for w in ingredients_str if not w in stop_words]
-    print(ingredients)
-    inverted_index = db.invertedindex.find()[0]
-    # ingredients = ["tomato","Carrot","cabbage"]
-    ranked_results = {}
-    final_result = []
-    result = []
+    try:
+        if re.search(r"\band\b",ingredients_str):
+            ingredients_str = re.sub("and","",ingredients_str)
+        if re.search(r"\bor\b",ingredients_str):
+            ingredients_str = re.sub("or","",ingredients_str)
 
-    for (count, input_ingred) in enumerate(ingredients):
-        #     print(count)
-        ingred_combine_list = []
-        word2 = Word(input_ingred.lower())
-        word1 = word2.lemmatize()
-        singular_word_str = Word(word1).singularize()
-        plural_word_str = Word(word1).pluralize()
-        if singular_word_str in inverted_index:
-            ingred_combine_list.extend(inverted_index[singular_word_str])
-        if plural_word_str in inverted_index:
-            ingred_combine_list.extend(inverted_index[plural_word_str])
-        if input_ingred.lower() in inverted_index:
-            if not final_result:
-                final_result.extend(set(ingred_combine_list))
-            else:
-                final_result = list(set(final_result) &
-                                    set(ingred_combine_list))
-                print(final_result)
+        ingredients_str = query
+        print(query)
+        ingredients_str = ingredients_str.split()
+        stop_words = set(["salt", "pepper"])
+        db = client.foodfood
+        ingredients = [w for w in ingredients_str if not w in stop_words]
+        print(ingredients)
+        inverted_index = db.invertedindex.find()[0]
+        # ingredients = ["tomato","Carrot","cabbage"]
+        ranked_results = {}
+        final_result = []
+        result = []
 
-            ranked_results["result"+str(count)] = final_result
-    #         print(ranked_results)
-    #         print("======================================================================================")
-    # print("====================================================")
-    # print(final_result)
-    # print("========================================================")
-    final_result1 = []
-    titlelist = []
-    for recipe in final_result:
-        
-        rec = db.reviews.find_one({"_id":recipe})
-        rec.pop("_id",None)
-        if rec["title"] not in titlelist:
-            titlelist.append(rec["title"])
-            final_result1.append(rec)
+        for (count, input_ingred) in enumerate(ingredients):
+            #     print(count)
+            ingred_combine_list = []
+            word2 = Word(input_ingred.lower())
+            word1 = word2.lemmatize()
+            singular_word_str = Word(word1).singularize()
+            plural_word_str = Word(word1).pluralize()
+            if singular_word_str in inverted_index:
+                ingred_combine_list.extend(inverted_index[singular_word_str])
+            if plural_word_str in inverted_index:
+                ingred_combine_list.extend(inverted_index[plural_word_str])
+            if input_ingred.lower() in inverted_index:
+                if not final_result:
+                    final_result.extend(set(ingred_combine_list))
+                else:
+                    final_result = list(set(final_result) &
+                                        set(ingred_combine_list))
+                    print(final_result)
+
+                ranked_results["result"+str(count)] = final_result
+        #         print(ranked_results)
+        #         print("======================================================================================")
+        # print("====================================================")
+        # print(final_result)
+        # print("========================================================")
+        final_result1 = []
+        titlelist = []
+        for recipe in final_result:
+            
+            rec = db.reviews.find_one({"_id":recipe})
+            rec.pop("_id",None)
+            if rec["title"] not in titlelist:
+                titlelist.append(rec["title"])
+                final_result1.append(rec)
 
 
-    print("=============================SP=======================")
-    # print(set(final_result1))
-    print("========================================================")
-    return ranked_results, final_result1
+        print("=============================SP=======================")
+        # print(set(final_result1))
+        print("========================================================")
+        return ranked_results, final_result1
+    except:
+        print("Recipes not found")
 
 
 app.run(host='127.0.0.1',port='5000',debug=True)
