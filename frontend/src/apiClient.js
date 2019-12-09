9a14b3b3cd007e9d2a842ac75cfeeb8430ce25ba
sheeryachavan <shreesh.chavan@gmail.com>
2019-12-09 18:11:55 -0500

    ipchange

--- frontend/src/apiClient.js
+++ frontend/src/apiClient.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
-const BASE_URI = 'http://35.170.249.243';
+const BASE_URI = 'http://3.83.65.164';
+// const BASE_URI = 'http://172.31.53.78';
 // const BASE_URI = 'http://localhost:5000';
 const client = axios.create({
     baseURL: BASE_URI,
     json: true,
-    data:{}
+    data: {}
 });
 class APIClient {
 
