cc84eac8470bba20447ebee477e2347677afc8dd
spatole12 <spatole@stevens.edu>
2019-12-13 22:56:43 -0500

    changes in inverted_index.py

--- backend/config.py
+++
@@ -1,7 +0,0 @@
-
-from pymongo import MongoClient
-import dns
-
-def MongoConnection(): 
-    client = MongoClient("mongodb+srv://<username>:<password>@combining-hku7y.mongodb.net/test?retryWrites=true&w=majority")
-    return client
--- backend/inverted_index.py
+++ backend/inverted_index.py
@@ -1,22 +1,27 @@
-def invertedIndex():
-    inverted_index ={}
-    docs=[]
-    with client:
-        
-        recipes = db.reviews.find()
-        
-        for recipe in recipes:
-            for ingred in recipe["key_ing"]:
+from pymongo import MongoClient
+import dns
+client = MongoClient("mongodb+srv://admin:admin@combining-hku7y.mongodb.net/test?retryWrites=true&w=majority")
+db = client.foodfood
 
-                ingredient_wordlist = ingred.split()
-                ingredient_wordlist = [word.lower() for word in ingredient_wordlist]
-                ingredient_wordlist = list(set(ingredient_wordlist))
- 
-                for ingred1 in ingredient_wordlist:
+inverted_index ={}
+docs=[]
+with client:
+    
+    recipes = db.reviews.find()
+    
+    for recipe in recipes:
+        for ingred in recipe["key_ing"]:
 
-                    if ingred1 not in inverted_index:
-                        inverted_index[ingred1] = []
-                    inverted_index[ingred1].append(recipe["title"])
-                docs.append(" ".join(ingred))
+            ingredient_wordlist = ingred.split()
+            ingredient_wordlist = [word.lower() for word in ingredient_wordlist]
+            ingredient_wordlist = list(set(ingredient_wordlist))
 
-    print(inverted_index)
+            for ingred1 in ingredient_wordlist:
+
+                if ingred1 not in inverted_index:
+                    inverted_index[ingred1] = []
+                inverted_index[ingred1].append(recipe["title"])
+            docs.append(" ".join(ingred))
+
+print(inverted_index)
+    # return inverted_index
